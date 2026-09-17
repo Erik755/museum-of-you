f65f7278551909f9be48a8cc6a077ede0287c371
Erik755 <52841743+Erik755@users.noreply.github.com>
2026-09-17 13:19:25 -0600

    fix: bump SW cache to v10, drop part-file assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "museum-you-v9";
+const CACHE = "museum-you-v10";
 const ASSETS = ["./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
