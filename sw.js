5e33bca8c6b7fb7af3aceff98440dd37ffb249f2
Erik755 <52841743+Erik755@users.noreply.github.com>
2026-09-17 17:45:09 -0600

    v25: auto-fallback to next Gemini model on any model error

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "museum-you-v24";
+const CACHE = "museum-you-v25";
 const ASSETS = ["./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon.svg", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
