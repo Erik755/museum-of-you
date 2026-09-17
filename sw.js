71f4154696c5ddf8206325635522a7fea0a15865
Erik755 <52841743+Erik755@users.noreply.github.com>
2026-09-17 14:29:19 -0600

    chore: SW v17 precache museum PNG icons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "museum-you-v16";
-const ASSETS = ["./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon.svg"];
+const CACHE = "museum-you-v17";
+const ASSETS = ["./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon.svg", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
   self.skipWaiting();
