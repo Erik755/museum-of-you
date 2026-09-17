1b37320e4d1c79236517d75a274e3e97b40b38e9
Erik755 <52841743+Erik755@users.noreply.github.com>
2026-09-17 10:44:25 -0600

    Actualizar cache para quitar el pie

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "museum-you-v3";
+const CACHE = "museum-you-v4";
 const ASSETS = ["./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
