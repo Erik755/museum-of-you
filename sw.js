489e47687e20b419495ac2b59852cb77199f992d
Erik755 <52841743+Erik755@users.noreply.github.com>
2026-09-17 14:56:45 -0600

    feat: artista en Nueva pieza, botón Cerrar, prompt con {{artista}} (v18)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "museum-you-v17";
+const CACHE = "museum-you-v18";
 const ASSETS = ["./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon.svg", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
